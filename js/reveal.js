388699e4cbc5b9fea17a33f67fb7f0c798b40f75
uyawer <uyawer@gmail.com>
2019-09-05 11:16:46 +0900

    ページ番号を画面にもURLにも表示させるよう修正

--- js/reveal.js
+++ js/reveal.js
@@ -80,7 +80,7 @@
 			// number for the current slide. The function needs to return an array
 			// with one string [slideNumber] or three strings [n1,delimiter,n2].
 			// See #formatSlideNumber().
-			slideNumber: false,
+			slideNumber: true,
 
 			// Can be used to limit the contexts in which the slide number appears
 			// - "all":      Always show the slide number
@@ -94,7 +94,7 @@
 
 			// Add the current slide number to the URL hash so that reloading the
 			// page/copying the URL will return you to the same slide
-			hash: false,
+			hash: true,
 
 			// Push each slide change to the browser history.  Implies `hash: true`
 			history: false,
